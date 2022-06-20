31dd81a46254dd9aea009358e1896f9201501ae3
Роман Петров <r4manpetrov@gmail.com>
2022-06-21 01:33:19 +0600

    feat: create base entity

---
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/model/Base.java
@@ -0,0 +1,19 @@
+package sbrf.practice.jsv.list.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class Base {
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date createdAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}

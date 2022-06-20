a9ca80d94dc98d96d592b167655eaf70e924a89e
Роман Петров <r4manpetrov@gmail.com>
2022-06-21 01:35:49 +0600

    feat: add other endpoints to user controller

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/UserController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/UserController.java
@@ -2,6 +2,8 @@ package sbrf.practice.jsv.list.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sbrf.practice.jsv.list.dto.CreateUserDto;
+import sbrf.practice.jsv.list.dto.UpdateUserDto;
 import sbrf.practice.jsv.list.model.User;
 import sbrf.practice.jsv.list.service.UserService;
 
@@ -28,8 +30,18 @@ public class UserController {
     }
 
     @PostMapping()
-    public User create(@RequestBody() User user) {
-        return this.service.create(user);
+    public User create(@RequestBody CreateUserDto dto) {
+        return this.service.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable("id") UUID id, @RequestBody UpdateUserDto dto) {
+        return this.service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") UUID id) {
+        this.service.deleteById(id);
     }
 
 }

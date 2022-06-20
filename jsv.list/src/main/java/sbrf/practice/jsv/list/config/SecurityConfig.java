3cc28f76664ff07db62e9783c62edecce382115a
Роман Петров <r4manpetrov@gmail.com>
2022-06-20 23:55:04 +0600

    feat: create security config

---
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package sbrf.practice.jsv.list.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+}

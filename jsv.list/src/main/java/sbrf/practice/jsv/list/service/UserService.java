0ddd563811b1de297102f0e9e5342a73444ffb39
Роман Петров <r4manpetrov@gmail.com>
2022-06-21 01:37:16 +0600

    feat: add other ops to service

--- jsv.list/src/main/java/sbrf/practice/jsv/list/service/UserService.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/service/UserService.java
@@ -2,6 +2,8 @@ package sbrf.practice.jsv.list.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sbrf.practice.jsv.list.dto.CreateUserDto;
+import sbrf.practice.jsv.list.dto.UpdateUserDto;
 import sbrf.practice.jsv.list.model.User;
 import sbrf.practice.jsv.list.repository.UserRepository;
 
@@ -22,12 +24,25 @@ public class UserService {
         return repository.findAll();
     }
 
-    public User create(User user) {
+    public User findById(UUID id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    }
+
+    public User create(CreateUserDto dto) {
+        User user = new User(dto.getUsername(), dto.getPassword());
         return repository.save(user);
     }
 
-    public User findById(UUID id) {
-        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    public User update(UUID id, UpdateUserDto dto) {
+        User user;
+        try {
+            user = findById(id);
+            user.setUsername(dto.getUsername());
+            user.setPassword(dto.getPassword());
+        } catch (EntityNotFoundException e) {
+            user = new User(dto.getUsername(), dto.getPassword());
+        }
+        return repository.save(user);
     }
 
     public void deleteById(UUID id) {

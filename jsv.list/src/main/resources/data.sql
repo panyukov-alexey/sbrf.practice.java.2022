5e7e08e8055cd72cdf22d19177e31290eff01603
behem0thik <behem0thik@users.noreply.github.com>
2022-06-22 00:49:21 +0600

    #2 add init table script

--- jsv.list/src/main/resources/data.sql
+++ jsv.list/src/main/resources/data.sql
@@ -1,3 +1,18 @@
+create table if not exists USERS
+(
+    ID       int auto_increment,
+    USERNAME varchar not null,
+    PASSWORD varchar not null,
+    constraint USERS_PK
+        primary key (ID)
+);
+
+create unique index USERS_ID_UINDEX
+    on USERS (ID);
+
+create unique index USERS_PASSWORD_UINDEX
+    on USERS (PASSWORD);
+
 INSERT INTO "USERS" ("USERNAME", "PASSWORD") VALUES ('Charlie Scene', 'Gdy7wQ');
 INSERT INTO "USERS" ("USERNAME", "PASSWORD") VALUES ('J-Dog', 'o8eQfF');
 INSERT INTO "USERS" ("USERNAME", "PASSWORD") VALUES ('Funny Man', 'YxeO22');

c3d0ad4128e736579a501c314a2663234168376c
Роман Петров <r4manpetrov@gmail.com>
2022-06-22 09:21:01 +0600

    fix: remove unnecessary permissions

--- jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package sbrf.practice.jsv.list.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
         httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
     }
 }

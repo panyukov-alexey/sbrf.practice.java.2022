697255263233bb37aef4d91e96a226bd7e40285b
Роман Петров <r4manpetrov@gmail.com>
2022-06-22 11:58:14 +0600

    feat: enable the opportunity for users in database to login

--- jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
@@ -1,13 +1,34 @@
 package sbrf.practice.jsv.list.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable();
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void initialize(AuthenticationManagerBuilder builder, DataSource dataSource) throws Exception {
+        builder.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,'true' as enabled from users where username=?")
+                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username=?")
+                .passwordEncoder(passwordEncoder);
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
 }

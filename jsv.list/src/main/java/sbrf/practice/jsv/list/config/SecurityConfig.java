72ad2c65a74cc2b57487b2dfa342f5b2b10e4316
Роман Петров <r4manpetrov@gmail.com>
2022-06-23 14:29:08 +0600

    fix: create password encoder bean

--- jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package sbrf.practice.jsv.list.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,18 +18,16 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    private PasswordEncoder passwordEncoder(){
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     };
-
     @Autowired
-    public void initialize(AuthenticationManagerBuilder builder, DataSource dataSource) throws Exception {
+    public void initialize( AuthenticationManagerBuilder builder, DataSource dataSource) throws Exception {
         builder.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("SELECT username,password,'true' as enabled FROM users WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username=?")
-                .passwordEncoder(passwordEncoder());
+                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username=?");
     }
 
     @Bean

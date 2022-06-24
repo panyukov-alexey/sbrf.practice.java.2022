bba9b365b74a2c587c047d16bbe2a893eb3ed765
Роман Петров <r4manpetrov@gmail.com>
2022-06-24 10:56:11 +0600

    feat: create file service

---
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/service/FileService.java
@@ -0,0 +1,54 @@
+package sbrf.practice.jsv.list.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sbrf.practice.jsv.list.dto.CreateFileDto;
+import sbrf.practice.jsv.list.dto.UpdateFileDto;
+import sbrf.practice.jsv.list.model.File;
+import sbrf.practice.jsv.list.repository.FileRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    private final FileRepository repository;
+
+    @Autowired
+    public FileService(FileRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<File> findAll() {
+        return repository.findAll();
+    }
+
+    public File findById(UUID id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    }
+
+    public File create(CreateFileDto dto) throws IOException {
+        File file = new File(dto.getUserId(), dto.getFile().getOriginalFilename(), dto.getFile().getBytes());
+        return repository.save(file);
+    }
+
+    public File update(UUID id, UpdateFileDto dto) throws IOException {
+        File file;
+        try {
+            file = findById(id);
+            file.setUserId(dto.getUserId());
+            file.setFilename(dto.getFile().getOriginalFilename());
+            file.setBinary(dto.getFile().getBytes());
+        } catch (EntityNotFoundException e) {
+            file = new File(dto.getUserId(), dto.getFile().getOriginalFilename(), dto.getFile().getBytes());
+        }
+        return repository.save(file);
+    }
+
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
+}

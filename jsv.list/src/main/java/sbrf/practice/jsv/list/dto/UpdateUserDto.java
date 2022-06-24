c603fac031efd29539d1ad224cd59b4035d518ba
Роман Петров <r4manpetrov@gmail.com>
2022-06-24 10:55:21 +0600

    fix: update user dto

--- jsv.list/src/main/java/sbrf/practice/jsv/list/dto/UpdateUserDto.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/dto/UpdateUserDto.java
@@ -1,8 +1,11 @@
 package sbrf.practice.jsv.list.dto;
 
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class UpdateUserDto extends CreateUserDto {
-    public UpdateUserDto(String username, String password) {
+    public UpdateUserDto(@NotNull String username, @NotNull @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") String password) {
         super(username, password);
     }
 }

6453a04ab7f8f53379439fd456c6d2712393b167
Роман Петров <r4manpetrov@gmail.com>
2022-06-26 00:37:43 +0600

    fix: refactor user entity

--- jsv.list/src/main/java/sbrf/practice/jsv/list/model/User.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/model/User.java
@@ -1,14 +1,16 @@
 package sbrf.practice.jsv.list.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,9 +22,13 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = false)
 public class User extends Base {
     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
     @Column(name = "id", updatable = false, nullable = false)
+    @Type(type = "uuid-char")
     @ColumnDefault("random_uuid()")
     private UUID id;
     @Column(name = "username", unique = true)
@@ -30,8 +36,10 @@ public class User extends Base {
     @Column(name = "password")
     private String password;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<File> files = new ArrayList<File>();
+    @OneToMany(mappedBy="author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Value("#{Collections.emptyList()}")
+    @JsonIgnore
+    private List<File> files;
 
     public User(String username, String password) {
         this.username = username;

aea2b06b13e9d3b761fe336f59255a6894b583e9
Роман Петров <r4manpetrov@gmail.com>
2022-06-26 00:38:27 +0600

    fix: refactor create file dto

--- jsv.list/src/main/java/sbrf/practice/jsv/list/dto/files/CreateFileDto.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/dto/files/CreateFileDto.java
@@ -1,32 +1,24 @@
 package sbrf.practice.jsv.list.dto.files;
 
-// import com.fasterxml.jackson.annotation.JsonProperty;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.Setter;
-import java.sql.Blob;
-import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
-public class CreateFileDto extends FileDto{
-    // @NotNull
-    // @JsonProperty("file_name")
-    // private String fileName;
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateFileDto {
 
-    // @NotNull
-    // @JsonProperty("author_id")
-    // private UUID authorID;
+    @NotNull
+    @JsonProperty("authorId")
+    private UUID authorId;
 
-    // @NotNull
-    // @JsonProperty("content")
-    // private Blob content;
-
-    public CreateFileDto(@NotNull String fileName, @NotNull UUID authorID, @NotNull Blob content) {
-        super(fileName, authorID, content);
-    }
-
-    // public CreateFileDto(String fileName, Blob content) {
-    //     this.fileName = fileName;
-    //     this.content = content;
-    // }
+    @NotNull
+    @JsonProperty("file")
+    private MultipartFile file;
 }

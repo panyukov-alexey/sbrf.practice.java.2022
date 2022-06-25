95c511ada3d5cd4e6090742060ed362eceecf823
Роман Петров <r4manpetrov@gmail.com>
2022-06-26 00:39:08 +0600

    fix: refactor user controller

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/UserController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/UserController.java
@@ -2,11 +2,10 @@ package sbrf.practice.jsv.list.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import sbrf.practice.jsv.list.dto.users.CreateUserDto;
 import sbrf.practice.jsv.list.dto.users.UpdateUserDto;
+import sbrf.practice.jsv.list.model.File;
 import sbrf.practice.jsv.list.model.User;
 import sbrf.practice.jsv.list.service.UserService;
 
@@ -23,31 +22,37 @@ public class UserController {
 
     @GetMapping()
     public List<User> findAll() {
-        log.info("Attempt to find all users");
+        log.info("Trying to get all existing users");
         return service.findAll();
     }
 
     @GetMapping("/{id}")
     public User findById(@PathVariable("id") UUID id) {
-        log.info("Attempt to find user with id={}", id);
+        log.info("Trying to get a file by given id={}", id);
         return service.findById(id);
+    }
+
+    @GetMapping("/{id}/files")
+    public List<File> findFilesById(@PathVariable("id") UUID id) {
+        log.info("Trying to get all files by given id={}", id);
+        return findById(id).getFiles();
     }
 
     @PostMapping()
     public User create(@Valid @RequestBody CreateUserDto dto) {
-        log.info("Attempt to create new user with username={} and password={}", dto.getUsername(), dto.getPassword());
+        log.info("Trying to create new user");
         return service.create(dto);
     }
 
     @PutMapping("/{id}")
     public User update(@PathVariable("id") UUID id, @Valid @RequestBody UpdateUserDto dto) {
-        log.info("Attempt to update user with id {}", id);
+        log.info("Trying to update a user with given id={}", id);
         return this.service.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") UUID id) {
-        log.info("Attempt to delete user with id {}", id);
+        log.info("Trying to delete a user with given id={}", id);
         service.deleteById(id);
     }
 

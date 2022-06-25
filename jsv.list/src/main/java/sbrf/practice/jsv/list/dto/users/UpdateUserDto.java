b271e4a4db70029d139b9bab4f9a0c108676bb32
Роман Петров <r4manpetrov@gmail.com>
2022-06-26 00:37:33 +0600

    fix: refactor update user dto

--- jsv.list/src/main/java/sbrf/practice/jsv/list/dto/users/UpdateUserDto.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/dto/users/UpdateUserDto.java
@@ -1,11 +1,10 @@
 package sbrf.practice.jsv.list.dto.users;
 
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public class UpdateUserDto extends CreateUserDto {
-    public UpdateUserDto(@NotNull String username, @NotNull @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") String password) {
+    public UpdateUserDto(String username, @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") String password) {
         super(username, password);
     }
 }

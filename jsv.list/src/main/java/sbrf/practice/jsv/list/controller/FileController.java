922d851742ec9f7b0d9e82fabdae15c38056b805
Роман Петров <r4manpetrov@gmail.com>
2022-06-28 12:33:16 +0600

    fix: move users endpoint to user controller and create new endpoint for downloading files

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/FileController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/FileController.java
@@ -2,6 +2,8 @@ package sbrf.practice.jsv.list.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
@@ -12,64 +14,62 @@ import sbrf.practice.jsv.list.model.File;
 import sbrf.practice.jsv.list.service.FileService;
 
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping()
+@RequestMapping("files")
 @RequiredArgsConstructor
 @Log4j2
 public class FileController {
 
-    private final FileService service;
+    private final FileService fileService;
 
-    @GetMapping("files")
+    @GetMapping()
     private List<FileDto> findAllFiles() throws IOException{
         log.info("Trying to get all existing files");
-        return service.findAllFiles();
+        return fileService.findAllFiles();
     }
 
-    @GetMapping("files/{id}")
+    @GetMapping("/{id}")
     private FileDto findFileById(@PathVariable("id") UUID id) throws IOException{
         log.info("Trying to get a file by given id={}", id);
-        return service.findFileById(id);
+        return fileService.findFileById(id);
 
     }
 
-    @GetMapping("users/{userId}/files")
-    private List<FileDto> findFilesByAuthor(@PathVariable("id") UUID authorId) throws IOException{
-       log.info("Trying to get all files the user with given id owns");
-       return service.findFilesByAuthor(authorId);
-    
-    }
-
-    @GetMapping("files/sorted")
+    @GetMapping("/sorted")
     private Page<FileDto> findAllSorted(@RequestParam("sort") Sort sort,
                                      @RequestParam("page") Integer page,
                                      @RequestParam("val") Integer valPerPage) throws IOException{
         log.info("Trying to get and sort all existing files by", sort);
-        return service.findAllSorted(sort, page, valPerPage);
+        return fileService.findAllSorted(sort, page, valPerPage);
     }
 
 
-    @PostMapping("files")
+    @PostMapping("/upload")
     public FileDto create(@Valid @ModelAttribute CreateFileDto dto) throws IOException {
         log.info("Trying to upload new file");
-        return service.create(dto);
+        return fileService.create(dto);
 
     }
 
-    @PutMapping("files/{id}")
+    @GetMapping("/{id}/download")
+    private byte[] downloadFileById(@PathVariable("id") UUID id) {
+        return fileService.downloadFileById(id);
+    }
+
+    @PutMapping("/{id}")
     public FileDto update(@PathVariable("id") UUID id, @Valid @ModelAttribute UpdateFileDto dto) throws IOException {
         log.info("Trying to update a file with given id={}", id);
-        return this.service.update(id, dto);
-
+        return fileService.update(id, dto);
     }
 
-    @DeleteMapping("files/{id}")
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") UUID id) {
         log.info("Trying to delete a file with given id={}", id);
-        service.deleteById(id);
+        fileService.deleteById(id);
     }
 }

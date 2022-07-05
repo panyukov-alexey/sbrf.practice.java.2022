22b4038dddeb40a645a3a69b629425b6974e4fc2
Роман Петров <r4manpetrov@gmail.com>
2022-07-06 02:43:09 +0600

    feat: add jquery

---
+++ jsv.list/src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+$(() => {
+    const url = window.location.origin;
+
+    $("#upload").submit((e) => {
+        e.preventDefault();
+        console.log("LELELE");
+        $.ajax({
+            url: `${url}/files`, type: "POST",
+            data: {  file: $(this).serialize()}
+        });
+    });
+
+    function remove(id) {
+        $.ajax({url: `${url}/files/${id}`, type: "DELETE"});
+    }
+});

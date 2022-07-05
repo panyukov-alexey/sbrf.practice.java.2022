84e3bbc8f3f8766e14a5f3407205a6e13454e9ce
Роман Петров <r4manpetrov@gmail.com>
2022-07-06 02:42:31 +0600

    fix: restructure home controller

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
@@ -4,39 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import sbrf.practice.jsv.list.dto.files.CreateFileDto;
 import sbrf.practice.jsv.list.dto.users.UserDto;
-import sbrf.practice.jsv.list.model.User;
 import sbrf.practice.jsv.list.service.FileService;
 import sbrf.practice.jsv.list.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.UUID;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @Autowired
     UserService userService;
     @Autowired
     FileService fileService;
-    @GetMapping("/home")
+    @GetMapping("/")
     public String homepage(Principal principal, Model model) throws IOException {
         String username = principal.getName();
         UserDto user = userService.findByUsername(username);
         model.addAttribute("username", username);
         model.addAttribute("files", fileService.findFilesByAuthor(user.getId()));
-        return "home";
+        return "index";
     }
+
     @PostMapping("/upload")
-    public String uploadFile(Principal principal, @ModelAttribute CreateFileDto dto, Model model) throws IOException {
+    public String upload(CreateFileDto dto, Principal principal, Model model) throws IOException {
         String username = principal.getName();
         UserDto user = userService.findByUsername(username);
         dto.setAuthorId(user.getId());
         fileService.create(dto);
-        return "redirect:/home";
+        return "redirect:/";
+    }
+
+    @GetMapping("/download/{id}")
+    public String download(@PathVariable("id") UUID id) {
+        return "redirect:/";
+    }
+
+    @PostMapping("/remove/{id}")
+    public String remove(@PathVariable("id") UUID id) {
+        fileService.deleteById(id);
+        return "redirect:/";
     }
 }

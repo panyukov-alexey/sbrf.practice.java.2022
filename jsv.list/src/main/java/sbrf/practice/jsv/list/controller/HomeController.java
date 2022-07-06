ff62a4c3c515554fd94d34b42b870267f233dae5
Роман Петров <r4manpetrov@gmail.com>
2022-07-07 02:29:01 +0600

    feat: add file downloading

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
@@ -1,5 +1,7 @@
 package sbrf.practice.jsv.list.controller;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import sbrf.practice.jsv.list.dto.files.CreateFileDto;
+import sbrf.practice.jsv.list.dto.files.FileDto;
 import sbrf.practice.jsv.list.dto.users.UserDto;
 import sbrf.practice.jsv.list.service.FileService;
 import sbrf.practice.jsv.list.service.UserService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.Principal;
 import java.util.UUID;
 
@@ -27,22 +33,25 @@ public class HomeController {
     public String homepage(Principal principal, Model model) throws IOException {
         String username = principal.getName();
         UserDto user = userService.findByUsername(username);
-        model.addAttribute("username", username);
+        model.addAttribute("user", user);
         model.addAttribute("files", fileService.findFilesByAuthor(user.getId()));
         return "index";
     }
 
     @PostMapping("/upload")
-    public String upload(CreateFileDto dto, Principal principal, Model model) throws IOException {
-        String username = principal.getName();
-        UserDto user = userService.findByUsername(username);
-        dto.setAuthorId(user.getId());
+    public String upload(CreateFileDto dto, Model model) throws IOException {
         fileService.create(dto);
         return "redirect:/";
     }
 
     @GetMapping("/download/{id}")
-    public String download(@PathVariable("id") UUID id) {
+    public String download(@PathVariable("id") UUID id, HttpServletResponse response) throws IOException {
+        FileDto file = fileService.findFileById(id);
+        byte[] content = fileService.downloadFileById(id);
+        IOUtils.copy(new ByteArrayInputStream(content), response.getOutputStream());
+        response.setContentType("application/json");
+        response.setHeader("Content-disposition", "attachment;filename=" + file.getFileName());
+        response.flushBuffer();
         return "redirect:/";
     }
 

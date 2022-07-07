683cc243cf20d534d832591a8d7ad467b32c19e5
Роман Петров <r4manpetrov@gmail.com>
2022-07-07 19:48:22 +0600

    fix: change styles and update page structure

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/HomeController.java
@@ -2,19 +2,19 @@ package sbrf.practice.jsv.list.controller;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import sbrf.practice.jsv.list.dto.files.CreateFileDto;
 import sbrf.practice.jsv.list.dto.files.FileDto;
+import sbrf.practice.jsv.list.dto.files.UpdateFileDto;
 import sbrf.practice.jsv.list.dto.users.UserDto;
 import sbrf.practice.jsv.list.service.FileService;
 import sbrf.practice.jsv.list.service.UserService;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.Principal;
@@ -37,25 +37,29 @@ public class HomeController {
         return "index";
     }
 
-    @PostMapping("/upload")
-    public String upload(CreateFileDto dto, Model model) throws IOException {
+
+    @RequestMapping(value="/edit", method=RequestMethod.POST, params="action=create")
+    public String create(@Valid CreateFileDto dto, Model model) throws IOException {
         fileService.create(dto);
         return "redirect:/";
     }
 
-    @GetMapping("/download/{id}")
-    public String download(@PathVariable("id") UUID id, HttpServletResponse response) throws IOException {
+    @RequestMapping(value="/edit", method=RequestMethod.POST, params="action=read")
+    public void read(@RequestParam("id") UUID id, HttpServletResponse response) throws IOException {
         FileDto file = fileService.findFileById(id);
         byte[] content = fileService.downloadFileById(id);
         IOUtils.copy(new ByteArrayInputStream(content), response.getOutputStream());
-        response.setContentType("application/json");
         response.setHeader("Content-disposition", "attachment;filename=" + file.getFileName());
-        response.flushBuffer();
+    }
+
+    @RequestMapping(value="/edit", method=RequestMethod.POST, params="action=update")
+    public String update(@RequestParam("id") UUID id, @Valid UpdateFileDto dto) throws IOException {
+        fileService.update(id, dto);
         return "redirect:/";
     }
 
-    @DeleteMapping("/remove/{id}")
-    public String remove(@PathVariable("id") UUID id) {
+    @RequestMapping(value="/edit", method=RequestMethod.POST, params="action=delete")
+    public String delete(@RequestParam("id") UUID id) {
         fileService.deleteById(id);
         return "redirect:/";
     }

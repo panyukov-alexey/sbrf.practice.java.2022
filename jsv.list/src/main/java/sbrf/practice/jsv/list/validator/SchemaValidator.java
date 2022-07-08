ab3bd8abd18e8b183e35e7d5c14ec0f11601551f
Роман Петров <r4manpetrov@gmail.com>
2022-07-08 23:11:43 +0600

    fix: now user cannot upload empty file

--- jsv.list/src/main/java/sbrf/practice/jsv/list/validator/SchemaValidator.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/validator/SchemaValidator.java
@@ -12,10 +12,11 @@ public class SchemaValidator implements ConstraintValidator<IsValidSchema, Multi
     @Override
     public boolean isValid(MultipartFile content, ConstraintValidatorContext context) {
         try {
-            String str = new String(content.getBytes());
+            byte[] bytes = content.getBytes();
+            String str = new String(bytes);
             ObjectMapper mapper = new ObjectMapper();
             JsonNode actualObj = mapper.readTree(str);
-            return true;
+            return !actualObj.isEmpty();
         } catch (Exception e) {
             return false;
         }

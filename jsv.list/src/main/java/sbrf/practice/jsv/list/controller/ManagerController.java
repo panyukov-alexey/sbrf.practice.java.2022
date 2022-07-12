8e99b03f66598897f66a7626f6984915de1891ac
Роман Петров <r4manpetrov@gmail.com>
2022-07-12 06:47:50 +0600

    fix: refactor manager controller structure

--- jsv.list/src/main/java/sbrf/practice/jsv/list/controller/ManagerController.java
+++ jsv.list/src/main/java/sbrf/practice/jsv/list/controller/ManagerController.java
@@ -4,19 +4,17 @@ import lombok.AllArgsConstructor;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sbrf.practice.jsv.list.dto.files.CreateFileDto;
 import sbrf.practice.jsv.list.dto.files.FileDto;
 import sbrf.practice.jsv.list.dto.files.UpdateFileDto;
 import sbrf.practice.jsv.list.dto.users.UserDto;
-import sbrf.practice.jsv.list.model.User;
 import sbrf.practice.jsv.list.service.FileService;
 import sbrf.practice.jsv.list.service.UserService;
 
@@ -24,7 +22,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @AllArgsConstructor
@@ -35,24 +35,65 @@ public class ManagerController {
 
     @GetMapping("/page/{id}")
     public String pageable(@PathVariable(name = "id") Integer pageNumber,
-                       @RequestParam(name = "search", defaultValue = "") String search,
-                       @RequestParam(name = "criteria", defaultValue = "id") String criteria,
-                       @RequestParam(name = "direction", defaultValue = "asc") String direction,
-                       Model model) {
-        final int size = 10;
-        boolean isAscending = direction.equalsIgnoreCase("asc");
-        UserDto user = (UserDto) model.getAttribute("user");
-        Page<FileDto> page = fileService.findByAuthorIdAndFilenameContains(user.getId(), search, PageRequest.of(pageNumber - 1, size, Sort.by(isAscending ? Sort.Direction.ASC : Sort.Direction.DESC, criteria)));
-        model.addAttribute("search", search);
-        model.addAttribute("sortCriteria", criteria);
+                           @RequestParam(name = "size", defaultValue = "10") Integer size,
+                           Model model) {
+        UserDto user = (UserDto)model.getAttribute("user");
+        Page<FileDto> page = fileService.findFilesByAuthor(user.getId(), PageRequest.of(pageNumber - 1, size));
         model.addAttribute("page", page);
-        model.addAttribute("isAscending", isAscending);
+        return "index";
+    }
+
+    @RequestMapping(value = "/page/{id}", params={"criteria", "direction"})
+    public String pageableWithSorting(@PathVariable(name = "id") Integer pageNumber,
+                                      @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                      @RequestParam(name = "criteria") String criteria,
+                                      @RequestParam(name = "direction") Sort.Direction direction,
+                                      Model model) {
+        Sort sort = Sort.by(direction, criteria);
+        Page<FileDto> page = fileService.findFilesByAuthor(user().getId(), PageRequest.of(pageNumber - 1, size, sort));
+        model.addAttribute("page", page);
+        model.addAttribute("criteria", criteria);
+        model.addAttribute("ascending", direction.isAscending());
+        return "index";
+    }
+
+    @RequestMapping(value = "/page/{id}", params={"search"})
+    public String pageableWithFiltering(@PathVariable(name = "id") Integer pageNumber,
+                                        @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                        @RequestParam(name = "search") String search,
+                                        Model model) {
+        if (search.isBlank()) {
+            return pageable(pageNumber, size, model);
+        }
+        Page<FileDto> page = fileService.findByAuthorIdAndFilenameContains(user().getId(), search, PageRequest.of(pageNumber - 1, size));
+        model.addAttribute("page", page);
+        model.addAttribute("search", search);
+        return "index";
+    }
+
+    @RequestMapping(value = "/page/{id}", params={"criteria", "direction", "search"})
+    public String pageableWithSortingAndFiltering(@PathVariable(name = "id") Integer pageNumber,
+                                                  @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                                  @RequestParam(name = "search") String search,
+                                                  @RequestParam(name = "criteria") String criteria,
+                                                  @RequestParam(name = "direction") Sort.Direction direction,
+                                                  Model model) {
+        if (search.isBlank()) {
+            return pageable(pageNumber, size, model);
+        }
+        Sort sort = Sort.by(direction, criteria);
+        Page<FileDto> page = fileService.findByAuthorIdAndFilenameContains(user().getId(), search,
+                PageRequest.of(pageNumber - 1, size, sort));
+        model.addAttribute("page", page);
+        model.addAttribute("criteria", criteria);
+        model.addAttribute("search", search);
+        model.addAttribute("ascending", direction.isAscending());
         return "index";
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        return pageable(1, "", "id", "asc", model);
+        return pageable(1, 10, model);
     }
 
 
